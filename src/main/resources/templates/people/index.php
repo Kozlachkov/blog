3e64796fc3a06ce2665ed9e3fd78bcad6599f3c7
Roman Kozlachkov <surleversant@hotmail.com>
2022-04-13 16:29:37 +0300

    Updated: SpringConfig and application.properties

---
+++ src/main/resources/templates/people/index.php
@@ -0,0 +1,117 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Keywords" content="Henry Roman Mart, Roman Kozlachkov" />
+<meta content="Помните, вы вломились ко мне в блог. Вытирайте ноги, когда будете топтать тут."/>
+<title>Henry Roman Mart</title>
+<link rel="icon" type="image/png" href="pics/index/favicon4.png">
+<link href="StyleCSS.css" rel="stylesheet" type="text/css" />
+<link href="App/lightbox/lightbox.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="App/lightbox/lightbox.js"></script>
+<script language="javascript"> 
+function HideArticleExtension(showHideDiv2,showHideDiv1) {
+	var ele = document.getElementById(showHideDiv2);
+	var ele2 = document.getElementById(showHideDiv1);
+	if(ele.style.display == "block") {	ele.style.display = "none";  ele2.style.display = "block"; }
+	else {	ele.style.display = "block"; ele2.style.display = "none"; }
+}
+function ButtonForum1 (i1){
+	var but1 = document.getElementById("butForum1");
+	var but2 = document.getElementById("butForum2");
+	if (i1 == 1) {but1.src= "pics/index/button1_1.png";}
+	else if (i1 == 2) {but2.src= "pics/index/button2_1.png";}
+}
+function ButtonForum2 (i2){
+	var but1 = document.getElementById("butForum1");
+	var but2 = document.getElementById("butForum2");
+	if (i2 == 1) {but1.src= "pics/index/button1.png";}
+	else if (i2 == 2) {but2.src= "pics/index/button2.png";}
+}
+function FRssImg1 (i3){
+	var but1 = document.getElementById("RssIconId");
+	if (i3 == 1) {but1.src= "App/rss/rss3.jpeg";}
+	else if (i3 == 2) {but1.src= "App/rss/rss2.jpeg";}
+}
+</script>
+<!-- Start VideoLightBox.com HEAD section -->
+<link rel="stylesheet" href="App/ref_videolb2015/videolightbox.css" type="text/css" />
+
+			<link rel="stylesheet" type="text/css" href="App/ref_videolb2015/overlay-minimal.css"/>
+			<script src="App/ref_videolb2015/jquery.js" type="text/javascript"></script>
+			<script src="App/ref_videolb2015/swfobject.js" type="text/javascript"></script>
+<!-- End VideoLightBox.com HEAD section -->
+</head>
+
+<body>
+
+<table width="1024px" border="0" align="center" cellpadding="0" cellspacing="0" >
+  <tr height="40px" >
+    <td width="400px" class="TextName"> HENRY ROMAN MART</td>
+    <td> <img src="pics/index/heading.jpg" width="624px"  height="40" /></td>
+  </tr>
+</table>
+<table  width="1024px" border="0" align="center" cellpadding="0" cellspacing="0">  
+<?php include("HEAD.php"); ?> 
+  <tr>
+    <td class="RightBorder1" align="center" valign="top">
+      <div class=" TextGeorgia">Помните, вы вломились ко мне в блог. Вытирайте ноги, когда будете топтать тут.</div><br />
+      <!--<div onmouseover="javascript:ButtonForum1('1')" onmouseout="javascript:ButtonForum2('1')"> 
+         <a href="http://www.romanmart.com/forum/" ><img id="butForum1" class="button_forum1" src="pics/index/button1.png" width="140" height="35" /></a><br />
+      </div>
+      <div onmouseover="javascript:ButtonForum1('2')" onmouseout="javascript:ButtonForum2('2')"> 
+         <a href="http://romanmart.com/chat/"> <img id="butForum2" class="button_forum1" src="pics/index/button2.png" width="140" height="35" /></a><br /><br />
+      </div>-->
+<div class="TextNotUnderlined1" style=" font-family: 'Times New Roman', Times, serif; font-size:14px; color:#999;">
+<a href="../App/comments/reg_form.php">Регистрация на комментарии.</a>
+</div><br />
+
+<span style=" font-family: 'Times New Roman', Times, serif; font-size:12px; color:#999;">
+Хроника сайта:<br />
+<iframe width="130" height="140" src="App/chronics.html"> </iframe>
+</span>
+   <div class="TextHonor">Сайт в долгой и бесконечной разработке, когда закончу, не знаю!</div>
+      
+    </td>
+    <td class="PaddingUsual1" align="left" valign="top"> 
+    <br/>
+    <?php include("Articles/Article2015.php"); ?> 
+    
+    </td>
+    <td class="LeftBorder1" align="center" valign="top">
+           <span class="TextHonor">их надо знать</span><br />
+       <a href="RightSide/Heroes/Heroes.php"><img src="RightSide/Heroes/pics/Iren_Sandler.jpeg" width="65" height="80" /></a>
+       <a href="RightSide/Heroes/Heroes.php"><img src="RightSide/Heroes/pics/Karapetian_large.jpg" width="65" height="80" /></a><br />
+<br />
+           <span class="TextHonor2">рекомендую</span><br />
+    <ul id="reccomend">
+      <li> <a href="http://dirty.ru/">dirty</a> </li>
+      <li> <a href="http://www.zdver.com/">зеленая дверь</a> </li>
+      <li> <a href="pics/apsni.jpg" rel="lightbox" >апсны</a> </li>
+    </ul>
+           <span class="TextHonor">интересные люди</span><br />
+       <a href="RightSide/People/people.php"><img src="RightSide/People/pics/Robert_Wood.png" width="65" height="80" /></a>
+       <a href="RightSide/People/people.php"><img src="RightSide/People/pics/aaron_Feuershtein.jpeg" width="65" height="80" /></a><br /> 
+       
+          <span class="TextHonor">интересное</span><br />
+       <a href="RightSide/Interesting/interesting.php"><img src="RightSide/Interesting/pics/sudan2.jpg" width="65" height="80" /></a>
+       <a href="RightSide/Interesting/interesting.php"><img src="RightSide/Interesting/pics/kov4eg.png" width="65" height="80" /></a><br />    
+
+          <span class="TextHonor">полезное</span><br />
+       <a href="RightSide/Useful/useful.php"><img src="RightSide/Useful/pics/Bonne.jpeg" width="65" height="80" /></a>
+       <a href="RightSide/Useful/useful.php"><img src="RightSide/Useful/pics/Okno.jpg" width="65" height="80" /></a><br />    
+    </td>
+  </tr>
+  <tr>
+    <td align="left" valign="top">&nbsp;</td>
+    <td valign="top">&nbsp;</td>
+    <td align="center" valign="top">&nbsp;</td>
+  </tr>
+</table>
+<script src="App/ref_videolb2015/jquery.tools.min.js" type="text/javascript"></script>
+<script src="App/ref_videolb2015/videolightbox.js" type="text/javascript"></script>
+
+
+</body>
+</html>
